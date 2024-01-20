dcdeeff9bfee3fe5edfbc908a07a988fcb32d051
Velnbur <kyrylo.baybula@distributedlab.com>
2024-01-20 23:18:21 +0200

    init

---
+++ apps/cli/src/main.rs
@@ -0,0 +1,5 @@
+use utils::add;
+
+fn main() {
+    println!("Hello, world! {}", add(1, 2));
+}
---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "rust-nix-monorepo-template";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
+
+    cargo2nix = {
+      url = "github:cargo2nix/cargo2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, flake-utils, nixpkgs, rust-overlay, cargo2nix, ... }:
+    flake-utils.lib.eachSystem [ "aarch64-linux" "aarch64-darwin" ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            rust-overlay.overlays.default
+            cargo2nix.overlays.default
+          ];
+        };
+
+        rustVersion = "1.72.0";
+
+        rust-toolchain = pkgs.rust-bin.stable."${rustVersion}".overrides {
+          extensions = [ "rust-src" "clippy" "rustfmt" ];
+        };
+
+        rustPkgs = pkgs.rustBuilder.makePackageSet {
+          inherit rustVersion;
+
+          packageFun = import ./Cargo.nix;
+        };
+
+        cli = (rustPkgs.workspace.cli { });
+      in
+      rec {
+        packages = {
+          inherit cli;
+          default = packages.cli;
+        };
+
+        apps = {
+          cli = flake-utils.lib.mkApp {
+            drv = packages.cli;
+          };
+          default = apps.cli;
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ rust-toolchain ];
+        };
+      }
+    );
+}
